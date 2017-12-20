9a1cc1dae67f8eaad342c757f492823c49ac9744
Jake Gadaleta <jake.gads@gmail.com>
2017-12-19 21:40:43 -0500

    Last of the night update

---
+++ The_Next_Project.py
@@ -0,0 +1,16 @@
+def main():
+    def text():
+        s = input("Please enter your text       ")
+        return s
+    def printText(str):
+        print("your text string is '%s'" % s)
+
+
+    s = text()
+    printText(s)
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -9,3 +9,8 @@ run = int(input("Do you want to run Test.py (0 for no, 1 for yes)    "))
 if run == 1:
     import Test
     Test.main()
+
+run = int(input("Do you want to run The_Next_Project.py (0 for no, 1 for yes)    "))
+if run == 1:
+    import The_Next_Project
+    The_Next_Project.main()
